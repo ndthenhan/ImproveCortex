4994ab96d541ab1772e057f4d086ccab988923b6
ndthenhan <ndthenhan@gmail.com>
2026-04-21 21:18:02 +0200

    add .sh for Ubuntu and .bat for Windows

---
+++ user_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#run : chmod +x user_backup.sh in terminal to make it executable
+# Copy user_restore path to CubeMX -> Project Manager -> User Code Generation -> Post generation -> Command: /home/thenhan/STM32/ImproveCortex/user_backup.sh
+PROJ_NAME=ImproveCortex #update in new projects
+PROJECT_ROOT="/home/thenhan/STM32/$PROJ_NAME"
+SAFE_HOUSE="/home/thenhan/STM32/BackupForCubeMX/$PROJ_NAME"
+TIMESTAMP=$(date +%Y%m%d_%H%M)
+
+# Add any new folders here, separated by a space
+#FOLDERS=("Utilities" "Drivers/Custom" "Docs")
+FOLDERS=("Utilities" )
+mkdir -p "$SAFE_HOUSE"
+
+for FOLDER in "${FOLDERS[@]}"; do
+    if [ -d "$PROJECT_ROOT/$FOLDER" ]; then
+        # -R ensures it keeps the directory structure inside the backup
+        rsync -avR "$PROJECT_ROOT/./$FOLDER" "$SAFE_HOUSE/"
+    fi
+done
+
+echo "$TIMESTAMP: Backed up folders for $PROJ_NAME" >> "$PROJECT_ROOT/CubeMX_regenerate_log.txt"
